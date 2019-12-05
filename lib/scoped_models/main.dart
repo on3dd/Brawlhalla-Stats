96870b868d90224f96c1b2d582b9eb2835b79cab
Artem <onedeadwave@gmail.com>
2019-12-05 16:52:22 +1000

    Updated scoped model

--- lib/scoped_models/main.dart
+++ lib/scoped_models/main.dart
@@ -1,6 +1,7 @@
 import 'package:brawlhalla_stats/classes/legend.dart';
 import 'package:brawlhalla_stats/classes/clan.dart';
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 import 'package:scoped_model/scoped_model.dart';
 import 'package:brawlhalla_stats/classes/player.dart';
 import 'package:http/http.dart' as http;
@@ -14,10 +15,11 @@ class MainModel extends Model {
   Player _player;
 
   void init(BuildContext context) async {
-		String data = await DefaultAssetBundle.of(context).loadString("assets/config.json");
-		var jsonResult = json.decode(data);
-		this._apiKey = jsonResult['api_key'];
-	}
+    String data =
+        await DefaultAssetBundle.of(context).loadString("assets/config.json");
+    var jsonResult = json.decode(data);
+    this._apiKey = jsonResult['api_key'];
+  }
 
   int get brawlhallaId => _brawlhallaId;
 
@@ -48,38 +50,53 @@ class MainModel extends Model {
     List<Legend> legends = [];
     for (final legend in data['legends']) {
       legends.add(new Legend(
-          legend['legend_id'],
-          legend['legend_name_key'],
-          legend['kos'],
-          legend['falls'],
-          legend['games'],
-          legend['wins'],
-          legend['level'],
+        legend['legend_id'],
+        legend['legend_name_key'],
+        legend['kos'],
+        legend['falls'],
+        legend['games'],
+        legend['wins'],
+        legend['level'],
+				'assets/images/legends/${legend['legend_name_key']}.png'
       ));
     }
-    
+
+    legends.sort((a, b) => b.level.compareTo(a.level));
+
     Clan clan = new Clan(
-        data['clan']['clan_name'],
-        data['clan']['clan_id'],
-        data['clan']['clan_xp'],
-        data['clan']['personal_xp'],
+      data['clan']['clan_name'],
+      data['clan']['clan_id'],
+      data['clan']['clan_xp'],
+      data['clan']['personal_xp'],
     );
 
     this._player = new Player(
-        data['brawlhalla_id'],
-        data['name'],
-        data['level'],
-        data['xp_percentage'],
-        data['games'],
-        data['wins'],
-        legends,
-        clan,
+      data['brawlhalla_id'],
+      data['name'],
+      data['level'],
+      data['xp_percentage'],
+      data['games'],
+      data['wins'],
+      legends,
+      clan,
     );
 
     return true;
   }
 
   double calculateWinrate() {
-  	return this._player.wins/this._player.games * 100;
-	}
+    return this._player.wins / this._player.games * 100;
+  }
+
+  Color getWinrateColor() {
+    var wr = this._player.wins / this._player.games * 100;
+    if (wr <= 25.0)
+      return Colors.red;
+    else if (wr >= 25.0 && wr <= 50.0)
+      return Colors.orange;
+    else if (wr >= 50.0 && wr <= 75.0)
+      return Colors.lightGreen;
+    else
+      return Colors.green;
+  }
 }
